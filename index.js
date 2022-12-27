380f2070dbce55b669d0de545ce7b1d23ec78499
Ibrahiem Mohammad <ibrahiem.mohammad@oracle.com>
2022-12-27 16:24:02 -0600

    download pokemon sprite

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 // This is a sample script to get data from PokeAPI
+// How to run
+// 1. Clone repository
+// 2. Run `node index.js <pokemon-name?>`
+const https = require('https');
+const fs = require('fs');
 
-console.log("Hello!")
+// parse pokemon name from arguments
+const pokemon = process.argv.slice(2)[0];
 
-let fetchRes = fetch("https://pokeapi.co/api/v2/pokemon/charmander");
+const spriteImageFileName = pokemon+"-sprite.png"
+const spriteImageFile = fs.createWriteStream(spriteImageFileName);
 
-fetchRes.then(res => res.json()).then(d => {
-    console.log(d)
-})
+// get pokemon data
+let fetchResponse = fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon);
+
+// download sprite from received data
+fetchResponse.then(response => response.json()).then(data => {
+    // console.log(data)
+    spriteUrl = data.sprites.front_default
+    // console.log(spriteUrl);
+
+    https.get(spriteUrl, function(response) {
+        response.pipe(spriteImageFile);
+    
+        spriteImageFile.on("finish", () => {
+            spriteImageFile.close();
+            console.log("Downloaded "+spriteImageFileName);
+        });
+    
+    });
+
+});
+
