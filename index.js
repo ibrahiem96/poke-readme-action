daa04e9f757215957c5ef9f92328ad9a2ee206b4
Ibrahiem Mohammad <ibrahiem.mohammad@oracle.com>
2022-12-27 19:47:17 -0600

    deploy action

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 // const https = require('https');
 // const fs = require('fs');
-const fetch = require('node-fetch')
+import fetch from 'node-fetch';
 const core = require('@actions/core');
 // const github = require('@actions/github');
 
