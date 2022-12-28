04ea13fa09245699b51f3cbe0bb66d72c3938b3d
Ibrahiem Mohammad <ibrahiem.mohammad@oracle.com>
2022-12-27 19:35:10 -0600

    deploy action

--- index.js
+++ index.js
@@ -2,33 +2,39 @@
 // How to run
 // 1. Clone repository
 // 2. Run `node index.js <pokemon-name?>`
-const https = require('https');
-const fs = require('fs');
 
-// parse pokemon name from arguments
-const pokemon = process.argv.slice(2)[0];
+// const https = require('https');
+// const fs = require('fs');
+const core = require('@actions/core');
+// const github = require('@actions/github');
 
-const spriteImageFileName = pokemon+"-sprite.png"
-const spriteImageFile = fs.createWriteStream(spriteImageFileName);
+// get pokemon name from input
+const pokemon = core.getInput('pokemon');
+// const pokemon = process.argv.slice(2)[0];
+console.log(pokemon)
+// const spriteImageFileName = pokemon+"-sprite.png"
+// const spriteImageFile = fs.createWriteStream(spriteImageFileName);
 
 // get pokemon data
 let fetchResponse = fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon);
 
 // download sprite from received data
 fetchResponse.then(response => response.json()).then(data => {
-    // console.log(data)
-    spriteUrl = data.sprites.front_default
+    console.log(data);
+    console.log(data.abilities);
+    core.setOutput("abilities", data.abilities);
+    // spriteUrl = data.sprites.front_default;
     // console.log(spriteUrl);
 
-    https.get(spriteUrl, function(response) {
-        response.pipe(spriteImageFile);
+    // https.get(spriteUrl, function(response) {
+    //     response.pipe(spriteImageFile);
     
-        spriteImageFile.on("finish", () => {
-            spriteImageFile.close();
-            console.log("Downloaded "+spriteImageFileName);
-        });
+    //     spriteImageFile.on("finish", () => {
+    //         spriteImageFile.close();
+    //         console.log("Downloaded "+spriteImageFileName);
+    //     });
     
-    });
+    // });
 
 });
 
