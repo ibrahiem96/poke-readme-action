ca229d624075057dbcfaa5ef477fcd3c52c65db6
Ibrahiem Mohammad <imohammad@flexera.com>
2022-12-28 19:12:44 -0600

    Dev/ibrahiem (#3)
    * test deploy action

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
-// This is a sample script to get data from PokeAPI
-// How to run
-// 1. Clone repository
-// 2. Run `node index.js <pokemon-name?>`
-const https = require('https');
-const fs = require('fs');
+const fetch = require('node-fetch');
+const core = require('@actions/core');
+const octocore = require("@octokit/core")
 
-// parse pokemon name from arguments
-const pokemon = process.argv.slice(2)[0];
+const repo = (repoName) => {
+    if (repoName.includes('/')) {
+        return repoName.split('/')[1]
+    }
+    else return repoName;
+};
 
-const spriteImageFileName = pokemon+"-sprite.png"
-const spriteImageFile = fs.createWriteStream(spriteImageFileName);
+const pokemon = core.getInput('POKEMON');
+const repo_owner = core.getInput('REPOSITORY_OWNER')
+const gh_token = core.getInput('GH_TOKEN');
+const commit_message = core.getInput('COMMIT_MESSAGE');
+const octocore_client = new octocore.Octokit({auth: gh_token})
+const repo_name = repo(core.getInput('REPOSITORY'));
 
-// get pokemon data
-let fetchResponse = fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon);
+console.log(pokemon)
 
-// download sprite from received data
-fetchResponse.then(response => response.json()).then(data => {
-    // console.log(data)
-    spriteUrl = data.sprites.front_default
-    // console.log(spriteUrl);
+function getReadme(){
+    // return octocore_client.request(`GET /repos/${repo_owner}/${repo_name}/contents/README.md?ref=dev`)
+    return octocore_client.request(`GET /repos/${repo_owner}/${repo_name}/contents/README.md`)
+}
 
-    https.get(spriteUrl, function(response) {
-        response.pipe(spriteImageFile);
-    
-        spriteImageFile.on("finish", () => {
-            spriteImageFile.close();
-            console.log("Downloaded "+spriteImageFileName);
+function updateReadme(spriteMarkdown){
+    getReadme().then(({ data }) => {
+        // console.log(data)
+        const rawContent = Buffer.from(data.content, data.encoding).toString();
+        const startIndex = rawContent.indexOf("<!--Pokemon Sprite-->")
+        const updatedContent = `${startIndex === -1 ? rawContent : rawContent.slice(0, startIndex)}\n${spriteMarkdown}`
+
+        octocore_client.request(`PUT /repos/${repo_owner}/${repo_name}/contents/README.md`, {
+            message: commit_message,
+            content: Buffer.from(updatedContent, "utf-8").toString(data.encoding),
+            path: "README.md",
+            sha: data.sha,
+            // branch: "dev",
+        })
+    })
+}
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        .then((response) => response.json())
+        .then((data) => { 
+            const spriteUrl = data.sprites.front_default;
+            const spriteMarkdown = `<!--Pokemon Sprite-->\n![image](${spriteUrl})`;
+
+            console.log(spriteMarkdown);
+
+            updateReadme(spriteMarkdown);
         });
-    
-    });
-
-});
-
